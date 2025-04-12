885995ef7ad0f751c784e591f93911e5e9d30bdf
Anderson <anderson.simioni22@gmail.com>
2025-04-12 10:18:23 -0300

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "os_core.h"
 
 int main(char* args, int args_len) {
-    std::cout << "Hello World!";
+    
+    OsCore osCore("proccess_file.txt... TODO: Get from args");
+    osCore.run();
+
     return 0;
 }
